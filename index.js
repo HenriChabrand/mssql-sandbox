c10b62238bc53e4b817231cbb26c26c2ebc5c7b9
HenriChabrand <HenriChabrand@users.noreply.github.com>
2017-10-31 18:21:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 const server = app.listen(process.env.PORT || 5000, () => {
     console.log('log: Express server listening on port %d in %s mode', server.address().port, app.settings.env);
